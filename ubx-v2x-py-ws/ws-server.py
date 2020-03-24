ed313a4852e9fc8bb0dd3b347c44529afae01741
botlabovics <61688913+botlabovics@users.noreply.github.com>
2020-03-24 12:59:57 +0100

    Update ws-server.py
    Commit próba

--- ubx-v2x-py-ws/ws-server.py
+++ ubx-v2x-py-ws/ws-server.py
@@ -13,7 +13,7 @@ async def hello(websocket, path):
     age = z["age"]
     city = z["city"]
 
-    greeting = f"Hello {name}, aged {age} living in {city}!"
+    greeting = f"Hello {name}, aged {age} living in {city}! Welcome here"
 
     await websocket.send(greeting)
     print(f"> {greeting}")
